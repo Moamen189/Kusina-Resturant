c08f72efd85354a46b03ac08f562ac2ed3532f17
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2022-10-14 10:01:43 +0200

    UpContactForm

---
+++ UmbracoProject3/Controllers/ContactForm.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+using System.Net.Mail;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.ActionResults;
+using Umbraco.Cms.Web.Website.Controllers;
+using UmbracoProject3.umbraco.models;
+
+namespace UmbracoProject3.Controllers
+{
+  
+    public class ContactForm : /*Surface*/Controller
+    {
+        //public ContactForm(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        //{
+        //}
+
+        [HttpPost]
+        public ActionResult SubmitForm(ContentForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mailMessage = new MailMessage(model.Email, "admin@admin.com");
+                mailMessage.Subject = model.Subject;
+                mailMessage.Body = model.Message;
+
+                SmtpClient smtpClient = new SmtpClient("127.0.0.1", 25);
+                smtpClient.Send(mailMessage);
+                TempData["ContactResult"] = "Mas been sent ...";
+                return View();
+            }
+            return View();
+
+           // return RedirectToCurrentUmbracoPage();
+
+        }
+    }
+}
---
+++ UmbracoProject3/umbraco/models/ContentForm.cs
@@ -0,0 +1,11 @@
+﻿namespace UmbracoProject3.umbraco.models
+{
+    public class ContentForm
+    {
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Subject { get; set; } = "";
+        public string Message { get; set; } = ""; 
+
+    }
+}
